2246a6b417132e64db0f264cfa69a45985412c4d
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 18:29:30 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
+
+def waiters
+  meals.map do |meal|
+    meal.waiter
+  end
+end
+
 end
