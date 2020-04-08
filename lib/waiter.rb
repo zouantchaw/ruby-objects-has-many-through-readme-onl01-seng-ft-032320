2828ef48423f0fcea5286ce6820114acd41c9325
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 18:20:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
 
 end
