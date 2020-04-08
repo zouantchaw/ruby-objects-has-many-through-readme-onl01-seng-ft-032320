89677de4c3421a679468aaca9ed636002c73c4b9
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 18:28:17 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,5 +16,11 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
+  
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+end
 
 end
