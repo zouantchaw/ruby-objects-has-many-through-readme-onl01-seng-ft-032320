a641e7b0c01db032a8fa9fa751f3352824fbc58b
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 18:19:30 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
-  
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
 end
